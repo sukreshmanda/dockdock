7cfd24a1eafb456b9389587b4600e45b5674de73
sukreshmanda <mandasukresh@gmail.com>
2022-09-12 13:34:50 +0530

    Refactor code

--- app.py
+++ app.py
@@ -5,21 +5,26 @@ STATE_JSON = "state.json"
 LEVELS = "levels"
 FILE = "file"
 INFO = "info"
+CONFIG_JSON = "game-config.json"
 
-config_json = "game-config.json"
-config_file = open(config_json)
-state_file = open(STATE_JSON)
 
-game_config = json.load(config_file)
-load_state = json.load(state_file)
+def load_game_configs():
+    config_file = open(CONFIG_JSON)
+    game_config = json.load(config_file)
+    config_file.close()
+    return game_config
 
-config_file.close()
-state_file.close()
 
-load_state["current_level"] = 0
+def load_state_from_file():
+    state_file = open(STATE_JSON)
+    load_state = json.load(state_file)
+    state_file.close()
+    return load_state
 
 
 def play_levels():
+    game_config = load_game_configs()
+    state = load_state_from_file()
     for level in game_config[LEVELS]:
         level_path = level[FILE]
         level_config = json.load(open(level_path))
@@ -33,4 +38,4 @@ def write_state(updated_state):
     conf.close()
 
 
-write_state(load_state)
+write_state()
