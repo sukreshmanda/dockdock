ada0036d79156436f6e790089d080c58d4996638
sukreshmanda <mandasukresh@gmail.com>
2022-09-15 10:41:22 +0530

    refactor code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
-from console_mapper import print_info_okblue
+from console_mapper import print_info_okblue, print_question_header
 
+QUESTION = "question"
 NAME = "name"
 STATE_JSON = "state.json"
 LEVELS = "levels"
@@ -29,7 +30,8 @@ def play_levels():
     for level in game_config[LEVELS]:
         level_path = level[FILE]
         level_config = json.load(open(level_path))
-        print_info_okblue(level_config[INFO], state[NAME], end="\n")
+        print_question_header("Question: "+level_config[QUESTION], end="\n")
+        print_info_okblue(level_config[INFO], end="\n")
 
 
 def write_state(updated_state):
